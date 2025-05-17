49c6a33024665294d373ff2e4434b2d30374104b
MD Waliullah <waliullah9099@gmail.com>
2025-05-17 16:02:27 +0600

    cc

--- src/app/modules/property/property.validation.ts
+++ src/app/modules/property/property.validation.ts
@@ -1,32 +1,34 @@
 import { z } from 'zod';
 
 const createPropertyValidationSchema = z.object({
-  title: z.string().min(1, { message: 'Property title is required!' }),
-  description: z
-    .string()
-    .min(1, { message: 'Property description is required!' }),
-  price: z.number({ required_error: 'Property price is required!' }),
-  images: z.array(z.string()).optional(),
-  thumbNailImage: z.string({
-    required_error: 'Property thumbnail image is required!',
+  body: z.object({
+    title: z.string().min(1, { message: 'Property title is required!' }),
+    description: z
+      .string()
+      .min(1, { message: 'Property description is required!' }),
+    price: z.number({ required_error: 'Property price is required!' }),
+    images: z.array(z.string()).optional(),
+    thumbNailImage: z.string({
+      required_error: 'Property thumbnail image is required!',
+    }),
+    type: z.enum(['Apartment', 'House', 'Land'], {
+      required_error: 'Property type is required!',
+    }),
+    propertyFor: z.enum(['For Sale', 'For Rent', 'Sold', 'Rented'], {
+      required_error: 'Property status is required!',
+    }),
+    bedrooms: z.number({ required_error: 'Number of bedrooms is required!' }),
+    bathrooms: z.number({ required_error: 'Number of bathrooms is required!' }),
+    city: z.string({ required_error: 'City is required!' }),
+    country: z.string({ required_error: 'Country is required!' }),
+    propertyAddress: z.string({
+      required_error: 'Property address is required!',
+    }),
+    squareFoot: z.number().optional(),
+    isFeatured: z.boolean().default(false),
+    buildYear: z.string().optional(),
+    status: z.enum(['pending', 'approved', 'rejected']).default('pending'),
   }),
-  type: z.enum(['Apartment', 'House', 'Land'], {
-    required_error: 'Property type is required!',
-  }),
-  propertyFor: z.enum(['For Sale', 'For Rent', 'Sold', 'Rented'], {
-    required_error: 'Property status is required!',
-  }),
-  bedrooms: z.number({ required_error: 'Number of bedrooms is required!' }),
-  bathrooms: z.number({ required_error: 'Number of bathrooms is required!' }),
-  city: z.string({ required_error: 'City is required!' }),
-  country: z.string({ required_error: 'Country is required!' }),
-  propertyAddress: z.string({
-    required_error: 'Property address is required!',
-  }),
-  squareFoot: z.number().optional(),
-  isFeatured: z.boolean().default(false),
-  buildYear: z.string().optional(),
-  status: z.enum(['pending', 'approved', 'rejected']).default('pending'),
 });
 
 const updatePropertyValidationSchema = z.object({
@@ -71,7 +73,10 @@ const updatePropertyValidationSchema = z.object({
   squareFoot: z.number().optional(),
   isFeatured: z.boolean().default(false),
   buildYear: z.string().optional(),
-  status: z.enum(['pending', 'approved', 'rejected']).default('pending').optional(),
+  status: z
+    .enum(['pending', 'approved', 'rejected'])
+    .default('pending')
+    .optional(),
 });
 
 export const propertyValidationSchema = {
